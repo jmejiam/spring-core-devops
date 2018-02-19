bdc518083ee0103db24bcb4ad689513554f5cc69
Jorge Mejia <jmejia@MacBook-Pro-de-Apple.local>
2018-02-19 14:44:38 -0600

    first commit

---
+++ src/test/java/guru/springframework/test/dstest/DataSourceTest.java
@@ -0,0 +1,36 @@
+package guru.springframework.test.dstest;
+
+/**
+ * Created by jt on 5/21/16.
+ */
+
+import guru.springframework.test.config.DataSourceConfig;
+import guru.springframework.test.ds.FakeDataSource;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes= DataSourceConfig.class)
+@ActiveProfiles("guru")
+@Ignore
+public class DataSourceTest {
+
+    private FakeDataSource fakeDataSource;
+
+    @Autowired
+    public void setFakeDataSource(FakeDataSource fakeDataSource) {
+        this.fakeDataSource = fakeDataSource;
+    }
+
+    @Test
+    public void TestDataSource() throws Exception {
+
+        System.out.println(fakeDataSource.getConnectionInfo());
+
+    }
+}
